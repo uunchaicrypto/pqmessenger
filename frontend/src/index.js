d628cd07cb5381443fa3e0fc946b764e9490c509
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-07-25 22:27:34 +0545

    chore : cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import {router} from './Routes/router';
 import {AuthContextProvider} from './context/AuthContext'
-import toast, { Toaster } from 'react-hot-toast';
+import  { Toaster } from 'react-hot-toast';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
